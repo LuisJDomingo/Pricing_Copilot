d49e522e655ffd240450b8418be56e0c3237633e
Luis J. Domingo <154591201+LuisJDomingo@users.noreply.github.com>
2024-01-29 10:48:45 +0100

    Update urls.py grafico y carga csv

--- proveedores/urls.py
+++ proveedores/urls.py
@@ -9,8 +9,10 @@ urlpatterns = [
     path('', views.proveedores, name='proveedores'),
     path('guardar_manualmente/', views.guardar_manualmente, name='guardar_manualmente'),
     path('inventariar/<int:id>', views.inventariar, name='inventariar'),
-    path('upload_csv/', views.upload_csv, name='upload_csv'),
+    path('importar_csv/', views.importar_csv, name='importar_csv'),
     path('autenticacion/login/', LoginView.as_view(), name='login'),
+    path('grafico_inventario/', views.grafico_inventario, name='grafico_inventario'),
+    path('eliminar_articulo/<int:id>/', views.eliminar_articulo, name='eliminar_articulo'),),
     
     
 ]
